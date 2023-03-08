5ad9a00cc6715c37a527c5f6bcab62f530230a80
sujeetchnp <sujeetchnp@gmail.com>
2023-03-07 21:28:37 -0500

    Fixed 404 not found

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" { // incase if user request r and if it's not /hello
-		http.Error(w, "JOKE", http.StatusNotFound)
+		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
 	if r.Method != "GET" { // When we type /hello in your browser by default it is GET method
